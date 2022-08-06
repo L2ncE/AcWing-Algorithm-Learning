096189a8f800ded96a4b245445f85e07b37a7f11
L2ncE <llance_24@foxmail.com>
2022-08-06 11:25:45 +0800

    feat:add floating dichotomy

---
+++ floating_dichotomy/floating_dichotomy.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 袁鑫浩 on 2022/8/6.
+//
+// 给定一个浮点数 x，求它的三次方根
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    cin >> x;
+
+    double l = -100, r = 100;
+    while (r - l > 1e-8)
+    {
+        double mid = (l + r) / 2;
+        if (mid * mid * mid >= x) r = mid;
+        else l = mid;
+    }
+
+    printf("%.6lf\n", l);
+    return 0;
+}
