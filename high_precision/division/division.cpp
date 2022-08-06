502798d241c27c0d1fe7f35cb5777defef993421
L2ncE <llance_24@foxmail.com>
2022-08-06 17:00:16 +0800

    feat:add high precision division

---
+++ high_precision/division/division.cpp
@@ -0,0 +1,39 @@
+//
+// Created by 袁鑫浩 on 2022/8/6.
+//
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> div(vector<int> &A, int b, int &r) {
+    vector<int> C;
+    r = 0;
+    for (int i = A.size() - 1; i >= 0; i--) {
+        r = r * 10 + A[i];
+        C.push_back(r / b);
+        r %= b;
+    }
+    reverse(C.begin(), C.end());
+    while (C.size() > 1 && C.back() == 0) C.pop_back();
+    return C;
+}
+
+int main() {
+    string a;
+    vector<int> A;
+
+    int B;
+    cin >> a >> B;
+    for (int i = a.size() - 1; i >= 0; i--) A.push_back(a[i] - '0');
+
+    int r;
+    auto C = div(A, B, r);
+
+    for (int i = C.size() - 1; i >= 0; i--) cout << C[i];
+
+    cout << endl << r << endl;
+
+    return 0;
+}
