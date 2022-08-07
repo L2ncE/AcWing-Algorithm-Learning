9bf8f86c40f37274dea4dd2f017aa7eb355bdf7a
L2ncE <llance_24@foxmail.com>
2022-08-07 09:05:53 +0800

    feat:add prefix sum

---
+++ prefix_sum&difference/prefix_sum/prefix_sum.cpp
@@ -0,0 +1,25 @@
+//
+// Created by 袁鑫浩 on 2022/8/7.
+//
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int n, m;
+int a[N], s[N];
+
+int main() {
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
+
+    for (int i = 1; i <= n; i++) s[i] = s[i - 1] + a[i];
+
+    while (m--) {
+        int l, r;
+        scanf("%d%d", &l, &r);
+        printf("%d\n", s[r] - s[l - 1]);
+    }
+    return 0;
+}
