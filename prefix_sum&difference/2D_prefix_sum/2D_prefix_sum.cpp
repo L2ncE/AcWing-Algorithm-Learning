60faf223a1f8bf87a33abfe69a6577039dba598e
L2ncE <llance_24@foxmail.com>
2022-08-07 09:13:08 +0800

    feat:add 2D prefix sum

---
+++ prefix_sum&difference/2D_prefix_sum/2D_prefix_sum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 1010;
+
+int n, m, q;
+int s[N][N];
+
+int main() {
+    scanf("%d%d%d", &n, &m, &q);
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            scanf("%d", &s[i][j]);
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            // 求前缀和
+            s[i][j] += s[i - 1][j] + s[i][j - 1] - s[i - 1][j - 1];
+
+    while (q--) {
+        int x1, y1, x2, y2;
+        scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
+        // 算子矩阵的和
+        printf("%d\n", s[x2][y2] - s[x1 - 1][y2] - s[x2][y1 - 1] + s[x1 - 1][y1 - 1]);
+    }
+
+    return 0;
+}
