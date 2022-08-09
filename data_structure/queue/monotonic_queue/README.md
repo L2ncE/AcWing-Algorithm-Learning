1fc6160594c9e2a8a56007885e62f488e1166754
L2ncE <llance_24@foxmail.com>
2022-08-09 11:18:41 +0800

    feat:add monotonic stack and queue

---
+++ data_structure/queue/monotonic_queue/README.md
@@ -0,0 +1,12 @@
+## 单调队列
+
+```c++
+常见模型：找出滑动窗口中的最大值/最小值
+int hh = 0, tt = -1;
+for (int i = 0; i < n; i ++ )
+{
+    while (hh <= tt && check_out(q[hh])) hh ++ ;  // 判断队头是否滑出窗口
+    while (hh <= tt && check(q[tt], i)) tt -- ;
+    q[ ++ tt] = i;
+}
+```
---
+++ data_structure/queue/simulation_queue/queue.cpp
@@ -0,0 +1,34 @@
+//
+// Created by 袁鑫浩 on 2022/8/9.
+//
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int m;
+int q[N], hh, tt = -1;
+
+int main()
+{
+    cin >> m;
+
+    while (m -- )
+    {
+        string op;
+        int x;
+
+        cin >> op;
+        if (op == "push")
+        {
+            cin >> x;
+            q[ ++ tt] = x;
+        }
+        else if (op == "pop") hh ++ ;
+        else if (op == "empty") cout << (hh <= tt ? "NO" : "YES") << endl;
+        else cout << q[hh] << endl;
+    }
+
+    return 0;
+}
