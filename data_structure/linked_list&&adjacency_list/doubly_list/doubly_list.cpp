f03b65e9269b89e550b1b5454433fd9faebaf28b
L2ncE <llance_24@foxmail.com>
2022-08-09 10:28:55 +0800

    feat:add doubly list

---
+++ data_structure/linked_list&&adjacency_list/doubly_list/doubly_list.cpp
@@ -0,0 +1,59 @@
+//
+// Created by 袁鑫浩 on 2022/8/9.
+//
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int m;
+int e[N], l[N], r[N], idx;
+
+// 在节点a的右边插入一个数x
+void insert(int a, int x) {
+    e[idx] = x;
+    l[idx] = a, r[idx] = r[a];
+    l[r[a]] = idx, r[a] = idx++;
+}
+
+// 删除节点a
+void remove(int a) {
+    l[r[a]] = l[a];
+    r[l[a]] = r[a];
+}
+
+int main() {
+    cin >> m;
+
+    // 0是左端点，1是右端点
+    r[0] = 1, l[1] = 0;
+    idx = 2;
+
+    while (m--) {
+        string op;
+        cin >> op;
+        int k, x;
+        if (op == "L") {
+            cin >> x;
+            insert(0, x);
+        } else if (op == "R") {
+            cin >> x;
+            insert(l[1], x);
+        } else if (op == "D") {
+            cin >> k;
+            remove(k + 1);
+        } else if (op == "IL") {
+            cin >> k >> x;
+            insert(l[k + 1], x);
+        } else {
+            cin >> k >> x;
+            insert(k + 1, x);
+        }
+    }
+
+    for (int i = r[0]; i != 1; i = r[i]) cout << e[i] << ' ';
+    cout << endl;
+
+    return 0;
+}
